cbb58e1463034853ca430cf7794f2f2f1ef2916a
manishshiwal <manishkumarshiwal@gmail.com>
2017-04-06 17:51:52 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,1 +1,9 @@
-
+a=int(raw_input("enter the number"))
+b=int(raw_input("enter the number"))
+c=int(raw_input("enter the number"))
+if(a>b and a>c):
+	print a
+if(b>a and b>c):
+	print b
+if (c>b and c>a):
+	print c
