12840cccdd5ec7fbe1e1d2fc7e1d8f152ed5a427
manishshiwal <manishkumarshiwal@gmail.com>
2017-04-07 04:33:52 +0530

    Create primeinterval.py

---
+++ primeinterval.py
@@ -0,0 +1,12 @@
+a=int(raw_input("enter the number"))
+b=int(raw_input("enter the number"))
+if(a==1 or a==0):
+	a=2
+for n in range(a,b):
+	s=0
+	for x in range(2,n):
+		if(n%x==0):
+			s=1
+			break
+	if(s==0):
+		print n
